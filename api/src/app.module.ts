83ab20f0a1d5c9e4c5d9ad7c0f9b4b11d78aab4e
Jamyth Luk <jamythluk@Jamyths-MacBook-Pro.local>
2021-04-12 15:02:23 +0800

    [REFACTOR]: api: remove static serve

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,10 +2,8 @@ import { Module } from '@nestjs/common';
 import { V1Module } from './v1/v1.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { SystemModule } from './system/system.module';
-import { AppController } from './app.controller';
 
 @Module({
   imports: [ScheduleModule.forRoot(), V1Module, SystemModule],
-  controllers: [AppController],
 })
 export class AppModule {}
